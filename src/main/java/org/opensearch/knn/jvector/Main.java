a267f15831f182abc5b89b9fcd57c012fd75cc36
Samuel Herman <sherman8915@gmail.com>
2025-02-12 16:27:43 -0800

    add all plugin boiler plate
    Signed-off-by: Samuel Herman <sherman8915@gmail.com>

---
+++ src/main/java/org/opensearch/knn/jvector/Main.java
@@ -0,0 +1,12 @@
+/*
+ *  Copyright OpenSearch Contributors
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.jvector;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/org/opensearch/knn/jvector/plugin/JVectorPlugin.java
@@ -0,0 +1,10 @@
+/*
+ *  Copyright OpenSearch Contributors
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.jvector.plugin;
+
+import org.opensearch.plugins.Plugin;
+
+public class JVectorPlugin extends Plugin {}
