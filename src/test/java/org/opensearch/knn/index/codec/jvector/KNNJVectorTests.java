4f7e8ca383668b62b8315d61c055074670dbca01
Samuel Herman <sherman8915@gmail.com>
2025-02-18 14:49:01 -0800

    add test for PQ with merges

--- src/test/java/org/opensearch/knn/index/codec/jvector/KNNJVectorTests.java
+++ src/test/java/org/opensearch/knn/index/codec/jvector/KNNJVectorTests.java
@@ -531,7 +531,6 @@ public class KNNJVectorTests extends LuceneTestCase {
 
     /**
      * Test the simple case of quantization where we have the perfect batch single batch size with no merges or too small batch sizes
-     * @throws IOException
      */
     @Test
     public void testJVectorKnnIndex_simpleCase_withQuantization() throws IOException {
@@ -685,4 +684,163 @@ public class KNNJVectorTests extends LuceneTestCase {
         }
     }
 
+    /**
+     * Test the simple case of quantization where we have the perfect batch single batch size each time with merges
+     */
+    @Test
+    public void testJVectorKnnIndex_happyCase_withQuantization_multipleMerges() throws IOException {
+        final int k = 3; // The number of nearest neighbours to gather
+        final int perfectBatchSize = 256; // 256 is the minimal batch size that will trigger a quantization without breaking it, generally speaking the batch size can't be lower than the number of clusters
+        final int totalNumberOfDocs = perfectBatchSize * 2;
+
+        IndexWriterConfig indexWriterConfig = LuceneTestCase.newIndexWriterConfig();
+        // TODO: re-enable this after fixing the compound file augmentation for JVector
+        indexWriterConfig.setUseCompoundFile(false);
+        indexWriterConfig.setCodec(new JVectorCodec(true));
+        indexWriterConfig.setMergePolicy(new ForceMergesOnlyMergePolicy());
+        // We set the below parameters to make sure no permature flush will occur, this way we can have a single segment, and we can force
+        // test the quantization case
+        indexWriterConfig.setMaxBufferedDocs(10000); // force flush every 10000 docs, this way we make sure that we only have a single
+        // segment for a totalNumberOfDocs < 1000
+        indexWriterConfig.setRAMPerThreadHardLimitMB(1000); // 1000MB per thread, this way we make sure that no premature flush will occur
+        final Path indexPath = createTempDir();
+        log.info("Index path: {}", indexPath);
+        try (
+                FSDirectory dir = new NIOFSDirectory(indexPath, FSLockFactory.getDefault());
+                IndexWriter w = new IndexWriter(dir, indexWriterConfig)
+        ) {
+            final float[] target = new float[] {
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f };
+            for (int i = 1; i < totalNumberOfDocs + 1; i++) {
+                final float[] source = new float[] {
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        i };
+                final Document doc = new Document();
+                doc.add(new KnnFloatVectorField("test_field", source, VectorSimilarityFunction.EUCLIDEAN));
+                w.addDocument(doc);
+                if (i % perfectBatchSize == 0) {
+                    w.commit();
+                }
+            }
+            log.info("Flushing docs to make them discoverable on the file system");
+            w.forceMerge(1);
+
+            try (IndexReader reader = DirectoryReader.open(w)) {
+                log.info("We should now have a single segment with {} documents", totalNumberOfDocs);
+                Assert.assertEquals(1, reader.getContext().leaves().size());
+                Assert.assertEquals(totalNumberOfDocs, reader.numDocs());
+
+                final Query filterQuery = new MatchAllDocsQuery();
+                final IndexSearcher searcher = newSearcher(reader);
+                KnnFloatVectorQuery knnFloatVectorQuery = new KnnFloatVectorQuery("test_field", target, k, filterQuery);
+                TopDocs topDocs = searcher.search(knnFloatVectorQuery, k);
+                assertEquals(k, topDocs.totalHits.value());
+                assertEquals(0, topDocs.scoreDocs[0].doc);
+                Assert.assertEquals(
+                        VectorSimilarityFunction.EUCLIDEAN.compare(
+                                target,
+                                new float[] {
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        1.0f }
+                        ),
+                        topDocs.scoreDocs[0].score,
+                        0.001f
+                );
+                assertEquals(1, topDocs.scoreDocs[1].doc);
+                Assert.assertEquals(
+                        VectorSimilarityFunction.EUCLIDEAN.compare(
+                                target,
+                                new float[] {
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        2.0f }
+                        ),
+                        topDocs.scoreDocs[1].score,
+                        0.001f
+                );
+                assertEquals(2, topDocs.scoreDocs[2].doc);
+                Assert.assertEquals(
+                        VectorSimilarityFunction.EUCLIDEAN.compare(
+                                target,
+                                new float[] {
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        0.0f,
+                                        3.0f }
+                        ),
+                        topDocs.scoreDocs[2].score,
+                        0.001f
+                );
+                log.info("successfully completed search tests");
+            }
+        }
+    }
 }
